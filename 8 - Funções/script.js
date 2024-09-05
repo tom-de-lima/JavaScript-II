e15d45a36047ea8e0f6ada8230249af0c0fd6d75
Tom Lima <antoniojosedelimaneto@gmail.com>
2024-09-05 18:41:26 -0300

    Aula Funções

---
+++ 8 - Funções/script.js
@@ -0,0 +1,15 @@
+// 1 - Declaração da Função
+
+function soma(a, b) {
+    return a + b;
+}
+
+function ola() {
+    console.log("Olá Mundo!");
+}
+
+
+// 2 - Chamada da Função criada
+
+console.log(soma(5, 3)); // Outputs: 8
+ola(); // Outputs: Olá Mundo
