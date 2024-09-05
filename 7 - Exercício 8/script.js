3839aafe535041395d1cbbe64bc0ffcbd1ae7e06
Tom Lima <antoniojosedelimaneto@gmail.com>
2024-09-05 18:17:01 -0300

    Melhoramento do programa

--- 7 - Exercício 8/script.js
+++ 7 - Exercício 8/script.js
@@ -7,16 +7,8 @@ let running = true
 // Looping
 
 while(running) {
-    
-    if(listaImoveis == 0){
-        alert("Nenhum imóvel cadastrado")
-    }
-    else{
-        alert("Quantidade de imóveis cadastrados no sistema: " + listaImoveis.length) 
-    }
-    
-
-    let menu = parseInt(prompt("1 - Adicionar um imóvel; " + "\n2 - Exibir imóveis cadastrados; " + "\n3 - Sair do progama."), 10) // parseInt com Radix: Quando você chama parseInt("08", 10), o segundo parâmetro 10 especifica explicitamente que a string deve ser interpretada como um número decimal. Isso garante que "08" seja sempre convertido para o número decimal 8, independentemente do navegador ou da versão do JavaScript.
+         
+    let menu = parseInt(prompt("Quantidade de imóveis cadastrados no sistema: " + listaImoveis.length + "\n1 - Adicionar um imóvel; " + "\n2 - Exibir imóveis cadastrados; " + "\n3 - Sair do progama."), 10) // parseInt com Radix: Quando você chama parseInt("08", 10), o segundo parâmetro 10 especifica explicitamente que a string deve ser interpretada como um número decimal. Isso garante que "08" seja sempre convertido para o número decimal 8, independentemente do navegador ou da versão do JavaScript.
 
     switch(menu){
         case 1:
@@ -26,7 +18,18 @@ while(running) {
                 n_banheiros: parseInt(prompt("Digite a quantidade de banheiros: "), 10),
                 garagem: prompt("Há garagem (sim ou não)? ").toLowerCase() === "sim" ? "sim" : "não" // "... === 'sim' ": Verifica se a resposta convertida é exatamente igual a "sim". Se a resposta for "sim", a expressão será true. Para qualquer outra resposta (como "não", "nao", "no", etc.), a expressão será false. Observe o uso do operador ternário para substituir o true por sim e o false por não.
             }
+
+            const confirmation = confirm("Deseja salvar o imóvel com as informações abaixo?" 
+                + "\nNome do Proprietário: " + novoImovel.nomeProprietario
+                + "\nQuantidade de quartos: " + novoImovel.n_quartos
+                + "\nQuantidade de banheiros: " + novoImovel.n_banheiros
+                + "\nHá garagem: " + novoImovel.garagem
+            )
+
+            if(confirmation) { // Se o usuário confirmar a operação
             listaImoveis.unshift(novoImovel) //Add elementos no início
+            alert("Imóvel salvo com sucesso!")
+            }
             break
             
         case 2:
@@ -35,7 +38,12 @@ while(running) {
             }
             else {
             for(let i = 0; i < listaImoveis.length; i++) {
-                alert("Imóvel " + (i + 1) + "\nNome do Proprietário: " + listaImoveis[i].nomeProprietario + "\nQuantidade de quartos: " + listaImoveis[i].n_quartos + "\nQuantidade de banheiros: " + listaImoveis[i].n_banheiros + "\nHá garagem: " + listaImoveis[i].garagem + "\n-----------------------------------------")
+                alert("Imóvel " + (i + 1) + 
+                "\nNome do Proprietário: " + listaImoveis[i].nomeProprietario + 
+                "\nQuantidade de quartos: " + listaImoveis[i].n_quartos + 
+                "\nQuantidade de banheiros: " + listaImoveis[i].n_banheiros + 
+                "\nHá garagem: " + listaImoveis[i].garagem + 
+                "\n-----------------------------------------")
                 }
             }
             break
