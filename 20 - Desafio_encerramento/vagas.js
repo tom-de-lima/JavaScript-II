d3f9462e0eaf504e39351b4508f7ec7cffa455c4
Tom Lima <antoniojosedelimaneto@gmail.com>
2024-10-10 10:06:13 -0300

    Função de visualização criada

--- 20 - Desafio_encerramento/vagas.js
+++ 20 - Desafio_encerramento/vagas.js
@@ -1,5 +1,6 @@
 const vagasDeEmprego = []
 
+
 //Função que cria a vaga
 function criarVaga(){
     let nome = prompt("Digite o nome da vaga:")
@@ -12,7 +13,8 @@ function criarVaga(){
         const vaga = {
             nome: nome,
             descricao: descricao,
-            dataLimite: dataLimite
+            dataLimite: dataLimite,
+            candidato: []
         }
         vagasDeEmprego.push(vaga)
         alert("Vaga criada com sucesso!")
@@ -28,29 +30,52 @@ function criarVaga(){
 function listarVagas(){
     if(vagasDeEmprego.length === 0){
         alert("Não há vagas disponíveis no momento.")
-}
+    }
 else {
+
+    let vagasEmTexto = "Vagas disponíveis no momento: \n\n"
+
+    //Aqui, vagasEmTexto += está fazendo o seguinte: Pega o valor atual da variável vagasEmTexto. Concatena (ou seja, junta) esse valor com o novo conteúdo (a string que contém informações da vaga, como nome, índice, etc.). Atribui o resultado de volta para a variável vagasEmTexto.
     for(let i = 0; i < vagasDeEmprego.length; i++){
-        alert("Índice da vaga: " + (i+1) + 
-        "\nNome da vaga: " + vagasDeEmprego[i].nome + "\nQuantidade de inscritos: " + inscritosPorvagas + "\n--------------------------------------------------------"
+        alert(vagasEmTexto += "Índice da vaga: " + i + 
+        "\nNome da vaga: " + vagasDeEmprego[i].nome +
+        "\nQuantidade de inscritos: " + vagasDeEmprego[i].candidatos.length + 
+        "\n--------------------------------------------------------"
         )
-}
-}
-}
-
-//------------------------------------------------------------------------------------------------------------------------
-
-//Função que mostra a quantidade de inscritos por vaga
-const inscritosPorvagas = vagasDeEmprego.reduce(function(acumulador, vaga) {
-    if(acumulador[vaga.nome]){
-        acumulador[vaga.nome]++
+        }
     }
-    else {
-        acumulador[vaga.nome] = 1
-    }
-    return acumulador
-}, 0)
+}
 
 //------------------------------------------------------------------------------------------------------------------------
 
 //Função para vizualizar uma vaga
+
+function visualizarVaga(){
+    let indexVaga = parseInt(prompt("Digite o índice da vaga que deseja visualizar:"), 10)
+    if(isNaN(indexVaga) || indexVaga < 0 || indexVaga >= vagasDeEmprego.length){
+        alert("Índice inválido. Tente novamente.")
+    }
+    else {
+        const vaga = vagasDeEmprego[indexVaga]
+
+        let detalhesDaVaga = "Nome da vaga: " + vaga.nome +
+                             "\nDescrição da vaga: " + vaga.descricao +
+                             "\nData limite para inscrição: " + vaga.dataLimite +
+                             "\nQuantidade de inscritos: " + vaga.candidatos.length +
+                             "\n--------------------------------------------------------"
+        
+        //Verificando se há nomes de candidatos inscritos
+        if(vaga.candidatos.length > 0){
+            detalhesDaVaga += "Candidatos inscritos na vaga: \n\n"
+
+            for(let i = 0; i < vaga.candidatos.length; i++){
+                detalhesDaVaga += (i + 1) + " " + vaga.candidatos[i] + "\n"
+            }
+        }
+        else{
+            detalhesDaVaga += "\nNenhum candidato inscrito na vaga."
+        }
+        
+        alert(detalhesDaVaga)
+    }
+}
