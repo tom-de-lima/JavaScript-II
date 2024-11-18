ec9ffca3204b3ad10bc9b04420d45e690aea3e49
Tom Lima <antoniojosedelimaneto@gmail.com>
2024-11-18 16:59:59 -0300

    Comentário novos

--- 11 - Escopo de funções/script.js
+++ 11 - Escopo de funções/script.js
@@ -1,26 +1,39 @@
 // Variável declarada no escopo mais externo do código:
-
 let nome = "João Lucas"
+// Declara uma variável chamada "nome" no escopo global, com o valor inicial "João Lucas".
+// Essa variável pode ser acessada e modificada por qualquer função neste escopo.
 
 function mudarNome() {
-    // Variável acessada dentro da função (escopo mais interno):
-    nome = "João Lucas Tavares de Lima"
+  // Variável acessada dentro da função (escopo mais interno):
+  nome = "João Lucas Tavares de Lima"
+  // Modifica o valor da variável global "nome" para "João Lucas Tavares de Lima".
+  // Como "nome" foi declarada no escopo global, essa alteração é refletida fora da função também.
 }
 
 function criarAnimal() {
-    let animal = {
-        nome: "Cachorro",
-        idade: 2,
-        cor: "Preto e branco",
-    }
-    
-    console.log(animal) //se tentar acessar a variável "animal" fora do escopo local da função será retornado um erro.
+  let animal = {
+    nome: "Cachorro",
+    idade: 2,
+    cor: "Preto e branco",
+  }
+  // Declara uma variável "animal" com escopo local dentro da função "criarAnimal".
+  // Essa variável é um objeto com propriedades: "nome", "idade" e "cor".
+
+  console.log(animal)
+  // Exibe o objeto "animal" no console.
+  // Se tentarmos acessar "animal" fora do escopo da função "criarAnimal", ocorrerá um erro,
+  // pois "animal" está disponível apenas no escopo local da função.
 }
-
-
 
 // Chamando funções
 
 console.log(nome)
+// Exibe o valor atual da variável global "nome" no console.
+// Saída esperada: "João Lucas"
+
 mudarNome()
+// Chama a função "mudarNome", que altera o valor da variável global "nome".
+
 console.log(nome)
+// Exibe o novo valor da variável global "nome" no console, após a execução de "mudarNome".
+// Saída esperada: "João Lucas Tavares de Lima"
--- 13 - Métodos/script.js
+++ 13 - Métodos/script.js
@@ -1,17 +1,30 @@
 let pessoa = {
-    nome: "",
-    idade: 0,
-    sexo: "",
-    metodo() { // Método criado dentro de um objeto
-        console.log("Nome: " + this.nome + ", Idade: " + this.idade + ", Sexo: " + this.sexo)
-    }
+  nome: "",
+  idade: 0,
+  sexo: "",
+  metodo() {
+    // Método criado dentro do objeto "pessoa".
+    console.log(
+      "Nome: " + this.nome + ", Idade: " + this.idade + ", Sexo: " + this.sexo
+    )
+    // Exibe as propriedades "nome", "idade" e "sexo" do objeto "pessoa".
+    // "this" faz referência ao próprio objeto "pessoa", acessando seus valores internos.
+  },
 }
+// Declara um objeto chamado "pessoa" com as propriedades "nome", "idade", e "sexo", e um método "metodo".
+// As propriedades são inicialmente vazias ou têm valores padrão (ex: idade = 0).
 
 // Acessando métodos de um objeto
 
 pessoa.nome = "João"
+// Define o valor da propriedade "nome" do objeto "pessoa" como "João".
+
 pessoa.idade = 25
+// Define o valor da propriedade "idade" do objeto "pessoa" como 25.
+
 pessoa.sexo = "Masculino"
+// Define o valor da propriedade "sexo" do objeto "pessoa" como "Masculino".
 
-pessoa.metodo() // Chamando o método da função pessoa
-
+pessoa.metodo()
+// Chama o método "metodo" do objeto "pessoa", que exibe as informações de "nome", "idade" e "sexo" no console.
+// Saída esperada: "Nome: João, Idade: 25
