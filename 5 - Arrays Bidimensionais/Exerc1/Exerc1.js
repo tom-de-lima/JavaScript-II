d9ceee71b09a55586f4b131532e8e44f45c31d8b
Tom Lima <97851202+tom-de-lima@users.noreply.github.com>
2025-02-08 13:03:40 -0300

    Vscode online teste

---
+++ 5 - Arrays Bidimensionais/Exerc1/Exerc1.js
@@ -0,0 +1,3 @@
+let matriz_3x3 = [[1,2,3], [4,5,6], [7,8,9]]
+
+console.log.table(matriz_3x3)
---
+++ 5 - Arrays Bidimensionais/Exerc2.js
@@ -0,0 +1,13 @@
+/*Exercício 2: Modificando Elementos da Matriz
+Descrição:
+Crie um programa que peça ao usuário para informar uma linha e uma coluna (de 0 a 2) e substitua o valor presente nessa posição da matriz por "X".
+
+Requisitos:
+
+Inicialize uma matriz 3x3 com números de 1 a 9.
+Peça ao usuário uma posição (linha e coluna) usando prompt().
+Substitua o valor na posição informada por "X".
+Exiba a matriz atualizada no console com console.table().
+ */
+
+let matriz_3x3 = []
