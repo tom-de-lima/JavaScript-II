d247d650b183c69d416ebdc0a5798d5100a10b48
Tom Lima <97851202+tom-de-lima@users.noreply.github.com>
2025-02-09 08:37:58 -0300

    Arrays bidimensionais

--- 5 - Arrays Bidimensionais/Exerc2.js
+++ 5 - Arrays Bidimensionais/Exerc2.js
@@ -10,4 +10,20 @@ Substitua o valor na posição informada por "X".
 Exiba a matriz atualizada no console com console.table().
  */
 
-let matriz_3x3 = []
+let matriz_3x3 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]
+
+console.log('Matriz com valores originais:')
+console.table(matriz_3x3)
+
+let valorSolicitado = parseInt(prompt('Digite un valor para ser acrescentado a matriz: '))
+let linha = parseInt(prompt('Digite a posição da linha que deseja modificar (0 a 2): '))
+let coluna = parseInt(prompt('Digite a posição da coluna que deseja modificar(0 a 2): '))
+
+matriz_3x3[linha].splice(coluna, 1, valorSolicitado)
+
+console.log('Matriz com valores alterados:')
+console.table(matriz_3x3)
---
+++ 5 - Arrays Bidimensionais/Exerc3.js
@@ -0,0 +1,25 @@
+/* Exercício 3: Somando os Elementos da Matriz
+Descrição:
+Crie um programa que some todos os elementos de uma matriz 3x3 e exiba o resultado no console.
+
+Requisitos:
+
+Declare uma matriz 3x3 contendo números inteiros.
+Percorra a matriz utilizando loops for aninhados.
+Calcule a soma de todos os elementos.
+Exiba o resultado final no console.*/
+
+let matriz_3x3 = [[10,4,76], [90,12,8], [23,34,78]]
+let somaDosElementos = 0
+
+console.log('Valores presentes na matriz:');
+console.table(matriz_3x3)
+
+
+for (let i = 0; i < matriz_3x3.length; i++) {
+    for (let j = 0; j < matriz_3x3[i].length; j++){
+        somaDosElementos += matriz_3x3[i][j]
+    }
+}
+
+console.log('Soma dos elementos da matriz: ', somaDosElementos)
