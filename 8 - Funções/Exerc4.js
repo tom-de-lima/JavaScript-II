6301d3d1142dc13870dbe8e0514fa40a8254cdf1
Tom de Lima <antoniojosedelimaneto@gmail.com>
2025-02-17 08:55:10 -0300

    Vai logo

--- 8 - Funções/Exerc4.js
+++ 8 - Funções/Exerc4.js
@@ -1,10 +1,10 @@
 /*tente modificar a função para permitir um intervalo de números e listar todos os pares dentro desse intervalo! z */
 
 function IntervaloEntreNumeros(numero1, numero2) {
-    
+
   let intervaloNumerico = []
 
-  //garante que o menor número fique armazenado na variável inicio e o maior número na variável fim. Isso é útil para situações onde precisamos definir um intervalo numérico ordenado, independentemente da ordem em que numero1 e numero2 são fornecidos.
+  //garante que o menor número fique armazenado na variável inicio e o maior número na variável fim. Isso é útil para situações onde precisamos definir um intervalo numérico ordenado, independentemente da ordem em que numero1 e numero2 são fornecidos. Bom
   let inicio = Math.min(numero1, numero2)
   let fim = Math.max(numero1, numero2)
 
